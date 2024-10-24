d7e6dbe9e5e4cfe84a089e913774765035f6174c
Debajyoti Das <136669104+CoderDebajyoti@users.noreply.github.com>
2024-10-24 22:20:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() !== "") {
+        var li = document.createElement("li");
+        li.textContent = taskInput.value;
+
+        var span = document.createElement("span");
+        span.textContent = " x";
+        span.classList.add("close");
+        span.onclick = function() {
+            var li = this.parentElement;
+            li.style.display = "none";
+        };
+
+        li.appendChild(span);
+        li.onclick = function() {
+            this.classList.toggle("completed");
+        };
+
+        taskList.appendChild(li);
+        taskInput.value = ""; // Clear the input field
+    }
+}
